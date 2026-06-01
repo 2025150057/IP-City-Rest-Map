6986934df677be57befd381626ff171d2a4acb66
hyaseae <hyaseae@gmail.com>
2026-06-01 21:48:48 +0900

    done with things to do in before runtime

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 async function test() {
-    const kakao_api_url = `https://dapi.kakao.com/v2/local/search/keyword?query=왕십리역`
+    const kakao_api_url = `https://dapi.kakao.com/v2/local/search/keyword?query=덕수궁길&size=5`
     const data = await fetch(kakao_api_url, {
         headers: {
             "content-type": "application/json;charset=UTF-8",
@@ -10,7 +10,14 @@ async function test() {
         }
     });
     const json = await data.json();
+
+    const address = json.documents[0].address_name;
+    const longitude = json.documents[0].x;
+    const latitude = json.documents[0].y;
+
+
     console.log(JSON.stringify(json, null, 2));
+    console.log(address, longitude, latitude);
 }
 test();
 
